1dbe1f9eb4e22e188344bb63bc6f6d3fba6480f8
camilalopes <kmilalopes10@gmail.com>
2018-12-30 23:37:51 -0200

    service worker on load event

--- js/sw_register.js
+++ js/sw_register.js
@@ -1,9 +1,11 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./sw.js')
-	.then(function() {
-		console.log('Service worker registered.');
-	})
-	.catch(function() {
-		console.log('Service worker registration failed!');
+	window.addEventListener('load', function() {
+		navigator.serviceWorker.register('./sw.js')
+		.then(function() {
+			console.log('Service worker registered.');
+		})
+		.catch(function() {
+			console.log('Service worker registration failed!');
+		});
 	});
 }
